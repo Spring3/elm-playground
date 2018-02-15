29bf88bd8fefda76334febad8c7b65a9b134d66e
Daniyil Vasylenko <danielspr3@gmail.com>
2018-02-15 04:40:05 +0200

    Add MUV items

--- fib/Main.elm
+++ fib/Main.elm
@@ -1,47 +1,88 @@
-import Html exposing (text)
+import Html exposing (beginnerProgram, div, input, text)
+import Html.Attributes exposing (placeholder, type_)
+import Html.Events exposing (onInput, onClick)
 import List
 import Tuple
-
-cache : List (Int, Int)
-cache = []
-
-inCache : Int -> Bool
-inCache num =
-  List.unzip cache
-  |> Tuple.first
-  |> List.member num
+import Debug
+import String
 
 toInt : Maybe Int -> Int
 toInt num =
   case num of
-    Nothing -> 0
     Just num -> num
+    _ -> 0
 
-getValue : Int -> Int
-getValue num =
-  List.filter (\(key, val) -> key == num) cache
+getValue : Int -> List (Int, Int) -> Maybe Int
+getValue num cache =
+  List.filter (\(key, value) -> key == num) cache
   |> List.head
   |> Maybe.map Tuple.second
-  |> toInt
 
-save : (Int, Int) -> List (Int, Int)
-save (key, val) = (key, val) :: cache
 
-fib : Int -> Int
-fib num =
+fib : Int -> Model -> Int
+fib num model =
   case num of
-    0 -> 1
+    0 -> 0
     1 -> 1
     _ ->
-      if inCache num then
-        getValue num
-      else
-        save (num, fib (num - 1) + fib (num - 2))
-        |> List.head
-        |> Maybe.map Tuple.second
-        |> toInt
+      let
+        fetchedValue = getValue num model.cache
+        value = case fetchedValue of
+          Nothing ->
+            (num, (fib ((num - 1) model) + fib ((num - 2) model)))::model.cache
+            |> List.head
+            |> Maybe.map Tuple.second
+            |> toInt
+
+          Just val -> val
+      in (value)
+
+oldfib : Int -> Int
+oldfib num =
+  case num of
+    0 -> 0
+    1 -> 1
+    _ -> fib (num - 1) + fib (num - 2)
+
+strToInt : String -> Int
+strToInt str =
+  case String.toInt str of
+    Ok num -> num
+    Err e -> 0 
+
+type alias Model = {
+  fibNum: Int,
+  cache: List (Int, Int),
+  result: Int
+}
+model : Model
+model = Model 0 [] 0
+
+type Msg = ValueChange String | Start
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    ValueChange newVal ->
+      { model | fibNum = strToInt newVal }
+    Start ->
+      { model |
+        result = fib(model.fibNum model)
+      }
+
+view : Model -> Html.Html Msg
+view model =
+  div []
+      [
+        input [ placeholder "Fibonacci", onInput ValueChange ] [],
+        div [] [ text (toString model.result) ],
+        input [ type_ "Submit", onClick Start ] []
+      ]
 
 main =
-  fib 40
-  |> toString
-  |> text
+  Html.beginnerProgram
+  {
+    model = model,
+    update = update,
+    view = view
+  }
+  
